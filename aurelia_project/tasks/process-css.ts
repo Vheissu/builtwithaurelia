c83e1f879a01fd2081bd08256ea3a3303b8ae676
vheissu <dwaynecharrington@gmail.com>
2016-07-28 10:43:49 +1000

    chore(all): added in sass and cssnano

--- aurelia_project/tasks/process-css.ts
+++ aurelia_project/tasks/process-css.ts
@@ -2,18 +2,23 @@ import * as gulp from 'gulp';
 import * as changedInPlace from 'gulp-changed-in-place';
 import * as sourcemaps from 'gulp-sourcemaps';
 import * as postcss from 'gulp-postcss';
+import * as sass from 'gulp-sass';
 import * as autoprefixer from 'autoprefixer';
+import * as cssnano from 'cssnano';
+
 import * as project from '../aurelia.json';
 import {build} from 'aurelia-cli';
 
 export default function processCSS() {
   let processors = [
-    autoprefixer({browsers: ['last 1 version']})
+    autoprefixer({browsers: ['last 1 version']}),
+    cssnano
   ];
 
   return gulp.src(project.cssProcessor.source)
     .pipe(changedInPlace({firstPass:true}))
     .pipe(sourcemaps.init())
     .pipe(postcss(processors))
+    .pipe(sass().on('error', sass.logError))
     .pipe(build.bundle());
 };
