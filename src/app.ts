63278e5aa06357601e81b2c5f0d050905c91c8b2
vheissu <dwaynecharrington@gmail.com>
2016-08-05 14:54:59 +1000

    feat(app): remove image from single view, button styling and UI

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,23 @@
 import {Aurelia, autoinject} from 'aurelia-framework';
 import {Router, RouterConfiguration} from 'aurelia-router';
+import {EventAggregator} from 'aurelia-event-aggregator';
 import {ApplicationService} from './services/application';
 
 @autoinject
 export class App {
+    ea: EventAggregator;
     appService: ApplicationService;
     router: Router;
 
     private showHat: boolean = false;
 
-    constructor(appService: ApplicationService) {
+    constructor(appService: ApplicationService, ea: EventAggregator) {
         this.appService = appService;
+        this.ea = ea;
+
+        this.ea.subscribe('router:navigation:success', payload => {
+            //this.send('pageview', payload.instruction.fragment, payload.instruction.title || document.title);
+        });
     }
 
     configureRouter(config: RouterConfiguration, router: Router) {
