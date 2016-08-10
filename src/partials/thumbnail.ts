538b1c818b3f9cf04e5f0001c0662265eb55453a
Dwayne Charrington <dwaynecharrington@gmail.com>
2016-08-10 23:31:04 +1000

    fix(app): voting buttons only visible to logged in users

--- src/partials/thumbnail.ts
+++ src/partials/thumbnail.ts
@@ -1,10 +1,18 @@
 import {autoinject, bindable, customElement} from 'aurelia-framework';
 
+import {UserService} from '../services/user';
+
 @autoinject
 @customElement('thumbnail')
 export class Thumbnail {
     @bindable project;
     @bindable voteCallback;
+
+    private userService: UserService;
+
+    constructor(userService: UserService) {
+        this.userService = userService;
+    }
 
     handleClick(url, name) {
         if ((<any>window).clicky) {
