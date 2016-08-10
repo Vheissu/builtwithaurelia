1e64c008d8e29f0a5640bb9ebc9af4d4b8a003d5
Dwayne Charrington <dwaynecharrington@gmail.com>
2016-08-10 23:20:31 +1000

    chore(app): clean thumbnail partial remove unneeded imports

--- src/partials/thumbnail.ts
+++ src/partials/thumbnail.ts
@@ -1,30 +1,10 @@
 import {autoinject, bindable, customElement} from 'aurelia-framework';
 
-import {UserService} from '../services/user';
-import {ApplicationService} from '../services/application';
-import {Api} from '../api';
-
 @autoinject
 @customElement('thumbnail')
 export class Thumbnail {
     @bindable project;
     @bindable voteCallback;
-
-    private api: Api;
-    private appService: ApplicationService;
-    private userService: UserService;
-
-    constructor(api: Api, appService: ApplicationService, userService: UserService) {
-        this.api = api;
-        this.appService = appService;
-        this.userService = userService;
-    }
-
-    getVoteCount(slug) {
-        return this.api.getVoteCountsBySlug(slug).then(count => {
-            return count || 0;
-        });
-    }
 
     handleClick(url, name) {
         if ((<any>window).clicky) {
