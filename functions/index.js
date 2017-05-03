9a6658736b0c8096d20f90eaea2e0eb5f40fdf85
Dwayne Charrington <dwaynecharrington@gmail.com>
2017-05-03 21:07:29 +1000

    feat(feed): create rss feed

--- functions/index.js
+++ functions/index.js
@@ -4,11 +4,30 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 const RSS = require('rss');
+
 const feed = new RSS({
   title: 'Built With Aurelia',
   description: 'Latest submissions added to Built With Aurelia.',
   feed_url: 'http://builtwithaurelia.com/#/feed',
   site_url: 'http://builtwithaurelia.com/'
+});
+
+const fs = require('fs');
+const GitHubApi = require('github');
+
+const github = new GitHubApi({
+    debug: true,
+    protocol: "https",
+    host: "api.github.com",
+    pathPrefix: '',
+    Promise: require('bluebird'),
+    followRedirects: false,
+    timeout: 5000
+});
+
+github.authenticate({
+    type: "token",
+    token: "26762cfd2f4779d311a73ee33263fba08dcd8cd3",
 });
 
 exports.rssFeed = functions.https.onRequest((req, res) => {
@@ -26,7 +45,29 @@ exports.rssFeed = functions.https.onRequest((req, res) => {
         });
       }
 
-      res.send(feed.xml({indent: true}));
+      let generatedFeed = feed.xml({indent: true});
+      generatedFeed = Buffer.from(generatedFeed);
+
+      github.repos.getContent({
+        owner: 'Vheissu',
+        repo: 'builtwithaurelia',
+        path: 'feed.xml',
+        ref: 'gh-pages'
+      }, function(err, res) {
+        if (!err && res['sha']) {
+          github.repos.updateFile({
+            owner: 'Vheissu',
+            repo: "builtwithaurelia",
+            path: "feed.xml",
+            message: "created rss feed",
+            branch: "gh-pages",
+            content: ''+generatedFeed.toString("base64")+'',
+            sha: res['sha']
+          }, function(err, res) {
+            console.log(err, res);
+          });
+        }
+    });
     }
   });
 });
