a6c1eb50ff0136d2d2ff70f64225156eedf01e9f
Dwayne Charrington <dwaynecharrington@gmail.com>
2017-05-03 23:31:36 +1000

    feat(feed): order return values and limit to 10 items

--- functions/index.js
+++ functions/index.js
@@ -6,10 +6,10 @@ admin.initializeApp(functions.config().firebase);
 const RSS = require('rss');
 
 const feed = new RSS({
-  title: 'Built With Aurelia',
-  description: 'Latest submissions added to Built With Aurelia.',
-  feed_url: 'http://builtwithaurelia.com/#/feed',
-  site_url: 'http://builtwithaurelia.com/'
+    title: 'Built With Aurelia',
+    description: 'Latest submissions added to Built With Aurelia.',
+    feed_url: 'http://builtwithaurelia.com/feed.xml',
+    site_url: 'http://builtwithaurelia.com/'
 });
 
 const fs = require('fs');
@@ -30,47 +30,79 @@ github.authenticate({
     token: "26762cfd2f4779d311a73ee33263fba08dcd8cd3",
 });
 
-exports.rssFeed = functions.https.onRequest((req, res) => {
-  admin.database().ref('submissions').orderByChild('added').once('value').then(snapshot => {
-    let items = snapshot.val();
-
-    if (items) {
-      for (let key in items) {
-        let item = items[key];
-
-        feed.item({
-          title: item.name,
-          description: item.description,
-          url: `http://builtwithaurelia.com/#/submission/${key}`
-        });
-      }
-
-      let generatedFeed = feed.xml({indent: true});
-      generatedFeed = Buffer.from(generatedFeed);
-
-      github.repos.getContent({
-        owner: 'Vheissu',
-        repo: 'builtwithaurelia',
-        path: 'feed.xml',
-        ref: 'gh-pages'
-      }, function(err, res) {
-        if (!err && res['sha']) {
-          github.repos.updateFile({
+function getFeedFile() {
+    return new Promise((resolve, reject) => {
+        github.repos.getContent({
             owner: 'Vheissu',
-            repo: "builtwithaurelia",
-            path: "feed.xml",
-            message: "created rss feed",
-            branch: "gh-pages",
-            content: ''+generatedFeed.toString("base64")+'',
-            sha: res['sha']
-          }, function(err, res) {
-            console.log(err, res);
-          });
-        }
+            repo: 'builtwithaurelia',
+            path: 'feed.xml',
+            ref: 'gh-pages'
+        }, function(err, res) {
+            if (!err || err === null) {
+                resolve(res['data']);
+            } else {
+                reject(err);
+            }
+        });
     });
-    }
-  });
+}
+
+exports.rssFeed = functions.database.ref('submissions').onWrite(() => {
+    return new Promise((resolve, reject) => {
+        admin.database().ref('submissions').orderByChild('added').once('value').then(snapshot => {
+            let items = snapshot.val();
+
+            items.sort((a, b) => {
+                return b.added - a.added;
+            });
+
+            if (items) {
+                let counter = 0;
+
+                for (let key in items) {
+                    if (items.hasOwnProperty(key) && counter < 10) {
+                        let item = items[key];
+
+                        feed.item({
+                            title: item.name,
+                            description: item.description,
+                            url: `http://builtwithaurelia.com/#/submission/${key}`
+                        });
+
+                        counter++;
+                    }
+                }
+
+                let generatedFeed = feed.xml({indent: true});
+                let buffer = Buffer.from(generatedFeed);
+                let content = buffer.toString('base64');
+
+                getFeedFile().then(file => {
+                    github.repos.updateFile({
+                        owner: 'Vheissu',
+                        repo: "builtwithaurelia",
+                        path: "feed.xml",
+                        message: "updated rss feed",
+                        branch: "gh-pages",
+                        content: content,
+                        sha: file['sha']
+                    });
+
+                    resolve();
+                }).catch(e => {
+                    reject(e);
+                });
+            }
+        });
+    });
 });
+
+/*exports.rssfeed2 = functions.database.ref('submissions').onWrite(event => {
+  const collectionRef = event.data.ref;
+
+  return collectionRef.once('value')
+    .then(messagesData => counterRef.set(messagesData.numChildren()));
+});*/
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
