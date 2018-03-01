7cc030507be35144e469ab53664ab2ece1ac97cf
Dwayne Charrington <dwaynecharrington@gmail.com>
2018-03-01 11:21:57 +1000

    feat(app): server side rendering works

--- functions/index.js
+++ functions/index.js
@@ -1,112 +1,62 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const { render } = require('aurelia-ssr-engine');
 
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-const RSS = require('rss');
+const app = new express();
+const bundle = './dist/server.bundle';
 
-const feed = new RSS({
-    title: 'Built With Aurelia',
-    description: 'Latest submissions added to Built With Aurelia.',
-    feed_url: 'http://builtwithaurelia.com/feed.xml',
-    site_url: 'http://builtwithaurelia.com/'
-});
+app.use(cors({ origin: true }));
 
-const fs = require('fs');
-const GitHubApi = require('github');
+app.use(express.static(path.resolve(__dirname)));
+app.use(express.static(path.resolve(__dirname, 'dist')));
 
-const github = new GitHubApi({
-    debug: true,
-    protocol: "https",
-    host: "api.github.com",
-    pathPrefix: '',
-    Promise: require('bluebird'),
-    followRedirects: false,
-    timeout: 5000
-});
+const renderApplication = (req, res, next) => {
+    const renderOptions = {
+        preboot: true,
+        prebootOptions: {
+            uglify: true
+        },
+        bundlePath: require.resolve(bundle),
+        template: require('fs').readFileSync(path.resolve('./dist/index.ssr.html'), 'utf-8')
+    };
 
-github.authenticate({
-    type: "token",
-    token: "26762cfd2f4779d311a73ee33263fba08dcd8cd3",
-});
+    const initializationOptions = {
+        main: () => {
+            delete require.cache[require.resolve(bundle)];
+            return require(bundle);
+        }
+    };
 
-function getFeedFile() {
-    return new Promise((resolve, reject) => {
-        github.repos.getContent({
-            owner: 'Vheissu',
-            repo: 'builtwithaurelia',
-            path: 'feed.xml',
-            ref: 'gh-pages'
-        }, function(err, res) {
-            if (!err || err === null) {
-                resolve(res['data']);
-            } else {
-                reject(err);
-            }
+    const url = `https://builtwithaurelia.com${req.path}`;
+
+    const extensionMatcher = /^.*\.[^\\]+$/;
+    if (req.path.match(extensionMatcher)) {
+        return next();
+    }
+
+    return render(Object.assign({ url }, renderOptions), initializationOptions)
+        .then(html => {
+            console.log('Render response', html);
+            return res.status(200).send(html);
+        })
+        .catch(e => {
+            return res.status(500).send(`<html><body>Failed to render ${req.path}</body></html>`);
+            console.log(`Failed to render ${req.path}`);
+            console.log(e);
         });
-    });
-}
+};
 
-exports.rssFeed = functions.database.ref('submissions').onWrite(() => {
-    return new Promise((resolve, reject) => {
-        admin.database().ref('submissions').orderByChild('added').once('value').then(snapshot => {
-            let items = snapshot.val();
-
-            // items.sort((a, b) => {
-            //     return b.added - a.added;
-            // });
-
-            if (items) {
-                let counter = 0;
-
-                for (let key in items) {
-                    if (items.hasOwnProperty(key) && counter < 10) {
-                        let item = items[key];
-
-                        feed.item({
-                            title: item.name,
-                            description: item.description,
-                            url: `http://builtwithaurelia.com/#/submission/${key}`
-                        });
-
-                        counter++;
-                    }
-                }
-
-                let generatedFeed = feed.xml({indent: true});
-                let buffer = Buffer.from(generatedFeed);
-                let content = buffer.toString('base64');
-
-                getFeedFile().then(file => {
-                    github.repos.updateFile({
-                        owner: 'Vheissu',
-                        repo: "builtwithaurelia",
-                        path: "feed.xml",
-                        message: "updated rss feed",
-                        branch: "gh-pages",
-                        content: content,
-                        sha: file['sha']
-                    });
-
-                    resolve();
-                }).catch(e => {
-                    reject(e);
-                });
-            }
-        });
-    });
+app.get('*', (req, res, next) => {
+    renderApplication(req, res, next);
 });
 
-/*exports.rssfeed2 = functions.database.ref('submissions').onWrite(event => {
-  const collectionRef = event.data.ref;
+const ssr = functions.https.onRequest(app);
 
-  return collectionRef.once('value')
-    .then(messagesData => counterRef.set(messagesData.numChildren()));
-});*/
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+module.exports = {
+    ssr
+};
