7ca451b01e169b4c98d9348f79f4373929e11e1b
Dwayne Charrington <dwaynecharrington@gmail.com>
2018-03-02 00:09:58 +1000

    feat(functions): rssfeed created

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,14 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const { render } = require('aurelia-ssr-engine');
+const RSS = require('rss');
+
+const feed = new RSS({
+    title: 'Built With Aurelia',
+    description: 'Latest submissions added to Built With Aurelia.',
+    feed_url: 'https://builtwithaurelia.com/feed.xml',
+    site_url: 'https://builtwithaurelia.com/'
+});
 
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
@@ -55,8 +63,44 @@ app.get('*', (req, res, next) => {
     renderApplication(req, res, next);
 });
 
+app.get('rss', (req, res) => {
+    functions.database.ref('submissions').onWrite(() => {
+        admin.database().ref('submissions').orderByChild('added').once('value').then(snapshot => {
+            let items = snapshot.val();
+
+            if (items) {
+                let counter = 0;
+
+                for (let key in items) {
+                    if (items.hasOwnProperty(key) && counter < 10) {
+                        let item = items[key];
+
+                        feed.item({
+                            title: item.name,
+                            description: item.description,
+                            url: `https://builtwithaurelia.com/submission/${key}`
+                        });
+
+                        counter++;
+                    }
+                }
+
+                let generatedFeed = feed.xml({indent: true});
+                let buffer = Buffer.from(generatedFeed);
+                let content = buffer.toString('base64');
+
+                res.status(200).send(content);
+            } else {
+                res.status(500);
+            }
+        });
+    });
+});
+
 const ssr = functions.https.onRequest(app);
+const rss = functions.https.onRequest(app);
 
 module.exports = {
-    ssr
+    ssr,
+    rss
 };
