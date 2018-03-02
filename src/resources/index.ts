053994f24374b1c32f9fe36c5eb3bcb11b3f3a14
Dwayne Charrington <dwaynecharrington@gmail.com>
2018-03-02 10:01:56 +1000

    chore(app): remove unneeded value converters

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -3,8 +3,6 @@ import {FrameworkConfiguration} from 'aurelia-framework';
 
 export function configure(config: FrameworkConfiguration) {
     config.globalResources([
-        PLATFORM.moduleName('./value-converters/async-binding-behavior'),
-        PLATFORM.moduleName('./value-converters/object-keys'),
-        PLATFORM.moduleName('./value-converters/prettify')
+        PLATFORM.moduleName('./value-converters/object-keys')
     ]);
 }
