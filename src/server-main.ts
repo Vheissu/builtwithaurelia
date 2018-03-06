06df8842a4b4584d29b47c2cc9a178705ab15c3c
Dwayne Charrington <dwaynecharrington@gmail.com>
2018-03-06 12:47:06 +1000

    refact(app): server entry doesn't need to completely duplicate

--- src/server-main.ts
+++ src/server-main.ts
@@ -1,17 +1,12 @@
 ﻿import { Aurelia } from 'aurelia-framework';
 import { PLATFORM } from 'aurelia-pal';
 import bootstrapper from 'aurelia-ssr-bootstrapper-webpack';
-import { initialState as initialStateCient } from './store/state';
-const serverInitialState = require('./store/prepopulated-state');
 
 (global as any).XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
 async function configure(aurelia: Aurelia) {
     aurelia.use
         .standardConfiguration();
-
-    aurelia.use.plugin(PLATFORM.moduleName('aurelia-animator-css'));
-    aurelia.use.plugin(PLATFORM.moduleName('aurelia-store'), { initialState: serverInitialState });
 
     await aurelia.start();
     await aurelia.setRoot(PLATFORM.moduleName('app'));
